e85b6f704fab2d04b889b1493957868e019ede16
akhil4393srm <73987041+akhil4393srm@users.noreply.github.com>
2021-11-30 22:06:22 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,7 @@
 import random
-min = 1
-max = 6
-roll_again = "yes"
-while roll_again == "yes" or roll_again == "y":
-    print "Rolling the dices..."
-    print "The values are...."
-    print random.randint(min, max)
-    print random.randint(min, max)
-    roll_again = raw_input("Roll the dices again?")
+roll_dice = input("comment start to roll the dice: ")
+
+if roll_dice == "start" or roll_dice=="START":
+   posiblle_results = [1, 2, 3, 4, 5, 6]
+   result = random.choice(posiblle_results)
+   print("Result of dice rolling is : " + str(result))
